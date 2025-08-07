aa112166379105a5ddb06c8373bf6eba52f66e89
정한별 <gksquf9720@naver.com>
2025-08-07 14:34:40 +0900

    onclick event 추가

--- react-todo-app/src/component/ToDo.tsx
+++ react-todo-app/src/component/ToDo.tsx
@@ -1,12 +1,33 @@
-import { IToDo } from "../atoms";
+import React from "react";
+import { IToDo, toDoState } from "../atoms";
+import { useSetRecoilState } from "recoil";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState)
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event.currentTarget.name)
+    const {
+      currentTarget: {name}
+    } = event
+    setToDos(oldToDos => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id)
+      const oldToDo = oldToDos[targetIndex]
+      const newToDo = { text, id, category: name}
+      return oldToDos
+    })
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>Doing</button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>Doing</button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>Doing</button>
+      )}
     </li>
   );
 }
